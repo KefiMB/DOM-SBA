59db7953ddaca356b260c94192f8ee9fc7c3b425
Kefilwe Magasa <kefi.magasa@gmail.com>
2024-05-05 20:06:39 -0400

    Document Fragment

--- script.js
+++ script.js
@@ -6,11 +6,16 @@ const emailInput = document.getElementById('email');
 form = document.querySelector('form');
 
 
-    const newParagraph = document.createElement('p');
-    const paragraphText = document.createTextNode('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sem lacus, iaculis sit amet consequat in, tincidunt eu mi. Etiam gravida rhoncus arcu in venenatis. Phasellus tempus magna libero, eu');
-    newParagraph.appendChild(paragraphText);
-  
+const newParagraph = document.createElement('p');
+const paragraphText = document.createTextNode('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sem lacus, iaculis sit amet consequat in, tincidunt eu mi. Etiam gravida rhoncus arcu in venenatis. Phasellus tempus magna libero, eu');
+newParagraph.appendChild(paragraphText);
 
 
+const template = document.getElementById('Template');
+const fragment = new DocumentFragment();
+const templateContent = template.content.cloneNode(true);
+fragment.appendChild(templateContent);
+const targetElement = document.getElementById('target');
+targetElement.appendChild(fragment);
 
-    
+
